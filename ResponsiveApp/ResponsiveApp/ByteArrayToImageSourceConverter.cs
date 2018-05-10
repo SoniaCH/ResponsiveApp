36cdd933d1c4eba0d89efdba3faccbdaefcbe6f3
SoniaChnini <chnini.sonia@gmail.com>
2018-05-10 10:11:36 +0100

    Add project files.

---
+++ ResponsiveApp/ResponsiveApp/ByteArrayToImageSourceConverter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ResponsiveApp
+{
+    public class ByteArrayToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageSource retSource = null;
+            try
+            {
+                if (value != null)
+                {
+                    byte[] imageAsBytes = (byte[])value;
+                    var stream = new MemoryStream(imageAsBytes);
+                    retSource = ImageSource.FromStream(() => stream);
+                }
+            }
+            catch (Exception e)
+            {
+
+                if (Debugger.IsAttached)
+                {
+                    Debug.WriteLine(e);
+                }
+                else
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            return retSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            byte[] data = null;
+
+            if (value != null)
+            {
+                try
+                {
+                    StreamImageSource streamImageSource = (StreamImageSource)(ImageSource)value;
+                    CancellationToken cancellationToken = CancellationToken.None;
+                    Task<Stream> task = streamImageSource.Stream(cancellationToken);
+                    Stream stream = Stream.Null;
+                    //stream.InitializeLifetimeService();
+                    stream = task.Result;
+                    MemoryStream ms = new MemoryStream();
+                    //ms.InitializeLifetimeService();
+                    stream.CopyTo(ms);
+                    data = ms.ToArray();
+                    return data;
+                }
+                catch (Exception e)
+                {
+                    if (Debugger.IsAttached)
+                    {
+                        Debug.WriteLine(e);
+                    }
+                    else
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            return data;
+        }
+    
+    }
+}
---
+++ ResponsiveApp/ResponsiveApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ResponsiveApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+           
+			InitializeComponent();
+            ByteArrayToImageSourceConverter converter = new ByteArrayToImageSourceConverter();
+
+            byte[] imgB = System.IO.File.ReadAllBytes("checkedimg.png");
+            ImageSource imgS= (ImageSource)converter.Convert(imgB, typeof(ImageSource), null, null);
+
+            imgSource.Source = imgS;
+            SizeChanged += MainPageSizeChanged;
+
+        }
+        void MainPageSizeChanged(object sender, EventArgs e)
+        {
+            imgSource.WidthRequest =this.Width/5;
+            bool isPortrait = this.Height < this.Width;
+            One.Orientation = (isPortrait ? StackOrientation.Horizontal : StackOrientation.Vertical);
+        }
+    }
+}
